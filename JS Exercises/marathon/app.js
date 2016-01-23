bd88e15fd3ff4e8b6cf8038c2b4a8a72f495b4c1
hann1187 <lhannum86@gmail.com>
2016-01-23 10:37:42 -0800

    homework tic tac toe

---
+++ JS Exercises/marathon/app.js
@@ -0,0 +1,51 @@
+// This is the marathon exercise
+var response = prompt("What was your best marathon time?");
+if (response > 4 && response <= 5) {
+  alert("Time was average");
+
+} else if (response > 2 && response <=4) {
+  alert("Time was excellent!");
+
+} else if (response > 5) {
+  alert("Speed up!");
+
+} else {
+  alert("Please enter a correct time.");
+}
+
+// This is Homework part 1 "For loop"
+
+for (var i = 0; i <= 20; i++) {
+	if (i % 2===0) {
+		console.log(i + 'is even');
+	} else {
+		console.log(i + 'is odd');
+	}
+}
+
+// This is Homework part 1 "While loop"
+
+while (i<=20) {
+
+}
+
+
+// This is Homework part 2: function Max
+
+var num1 = 6;
+var num2 = 10;
+
+var maxNum = Math.max(num1, num2);
+
+if (maxNum==num1){
+	alert(num1);
+}
+else if (maxNum==num2){
+	alert(num2)
+}
+
+
+
+
+
+
