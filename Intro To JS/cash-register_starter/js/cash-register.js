80d478222eeb73259b2b04a56b85954047251dd1
hann1187 <lhannum86@gmail.com>
2016-02-04 21:28:54 -0800

    added folders to FEWD 39 folder

---
+++ Intro To JS/cash-register_starter/js/cash-register.js
@@ -0,0 +1,44 @@
+// Structure
+//hint: form, entries, input, total
+// ------------------------------------------------
+
+
+
+// Setup
+// hint: what's the initial value of the total value?
+// ------------------------------------------------
+
+
+
+// Events
+// hint: when user hits "submit", add an event listener on the form
+// ------------------------------------------------
+
+
+
+// Event handler functions
+// comments below are only meant as suggestions.  There are many ways to creating this function.
+
+// ------------------------------------------------
+function enter(event) {
+	event.preventDefault();
+	// get the current entry value, convert to number with parseFloat
+	
+
+  	// update the value for the total
+	
+
+  	// create and append the new list item
+	
+
+  	// set the text content for both the new list item and the total
+	
+	
+	// clean up!  Can we clear the value in the input field?
+	
+}
+
+// Utility functions
+// in case you want to create any additional functions?
+// hint:   a function to format the currency?
+// ------------------------------------------------
