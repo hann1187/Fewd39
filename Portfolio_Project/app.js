f3ebfa6d3512be151e171e3adaefad08b88d6694
hann1187 <lhannum86@gmail.com>
2016-04-30 14:25:18 -0700

    changed the layout of my final project
    New image, new about me and photo, new content.

--- Portfolio_Project/app.js
+++ Portfolio_Project/app.js
@@ -1,7 +1,7 @@
-
+// Masonry javascript//
 <script src="/path/to/masonry.pkgd.min.js"></script>
 
-
+// Scroll Spy jquery//
 $('body').scrollspy({ target: '.navbar navbar-default navbar-fixed-top' })
 
 
